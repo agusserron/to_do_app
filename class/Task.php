fa0d70de610f89589b36e18c40e817bb34567fc0
agusserron <agusserron@gmail.com>
2024-09-07 17:19:13 -0300

    Refactoring clase Task

--- class/Task.php
+++ class/Task.php
@@ -3,47 +3,49 @@
 class Task {
     private $apiUrl = "https://jsonplaceholder.typicode.com/todos";
 
-    public function getAllTasks() {
+    
+    private function callApi($url, $method = 'GET', $data = null) {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->apiUrl);
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        
+        if ($data) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                'Content-Type: application/json'
+            ]);
+        }
+
         $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            echo 'Error:' . curl_error($ch);
+        }
+
         curl_close($ch);
         return json_decode($response, true);
+    }
+    
+    
+    
+    public function getAllTasks() {
+      return $this->callApi($this->apiUrl);
     }
 
     public function updateTaskStatus($taskId, $completed) {
         $url = $this->apiUrl . '/' . $taskId;
-    
         $data = json_encode(['completed' => $completed]);
     
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PATCH");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Content-Type: application/json'
-        ]);
-        $response = curl_exec($ch);
-    
-        if (curl_errno($ch)) {
-            echo 'Error:' . curl_error($ch);
-        }
-    
-        curl_close($ch);
-        return json_decode($response, true);
+        return $this->callApi($url, 'PATCH', $data);
     }
     
 
     public function deleteTask($taskId) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->apiUrl . "/$taskId");
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
-        curl_close($ch);
-        return json_decode($response, true);
+        $url = $this->apiUrl . '/' . $taskId;
+    
+        return $this->callApi($url, 'DELETE');
     }
     
 }
